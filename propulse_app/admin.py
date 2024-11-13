9a6b4515ca73ade564611684639399aaf55e2f07
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-13 07:37:06 +0100

    registered model

--- propulse_app/admin.py
+++ propulse_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Utility, HostelMedia, Hostel
 
 # Register your models here.
+
+admin.site.register(Utility)
+admin.site.register(HostelMedia)
+admin.site.register(Hostel)
--- propulse_app/models.py
+++ propulse_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 class Utility(models.Model):
 	name = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = "Utilities"
 	
 	def __str__(self):
 		return self.name
