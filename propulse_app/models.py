4dbd72094c6c01d56f8c3d47263b6373e7e17bfe
Abdul12majid <yisaabdulmajid@gmail.com>
2024-11-13 07:46:44 +0100

    made media field unrequired

--- propulse_app/models.py
+++ propulse_app/models.py
@@ -13,8 +13,8 @@ class Utility(models.Model):
 
 class HostelMedia(models.Model):
 	hostel = models.ForeignKey('Hostel', on_delete=models.CASCADE)
-	video = models.FileField(upload_to='hostel_videos/')
-	picture = models.ImageField(upload_to='hostel_pictures/')
+	video = models.FileField(blank=True, null=True, upload_to='hostel_videos/')
+	picture = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
 
 	def __str__(self):
 		return self.hostel.name + ' media.'
@@ -25,12 +25,18 @@ class Hostel(models.Model):
 	property_size = models.CharField(max_length=200)
 	distance_to_junction = models.CharField(max_length=500)
 	utilities = models.ForeignKey('Utility', on_delete=models.CASCADE)
-	picture1 = models.ImageField(upload_to='hostel_pictures/')
-	picture2 = models.ImageField(upload_to='hostel_pictures/')
-	picture3 = models.ImageField(upload_to='hostel_pictures/')
-	picture4 = models.ImageField(upload_to='hostel_pictures/')
-	picture5 = models.ImageField(upload_to='hostel_pictures/')
-	more_media = models.ForeignKey('HostelMedia', related_name='get_hostels',  on_delete=models.CASCADE)
+	picture1 = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
+	picture2 = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
+	picture3 = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
+	picture4 = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
+	picture5 = models.ImageField(blank=True, null=True, upload_to='hostel_pictures/')
+	more_media = models.ForeignKey(
+		'HostelMedia', 
+		related_name='get_hostels',  
+		on_delete=models.CASCADE,
+		blank=True, 
+		null=True
+		)
 
 	
 	def __str__(self):
